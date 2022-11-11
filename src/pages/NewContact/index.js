376a4ceb8ab558e90d57dea9547a107ab49e0184
Daniel Kadã <dgames201615@outlook.com>
2022-11-11 04:57:07 -0300

    added container/presentational pattern with custom hooks (NewContact)

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,33 +1,12 @@
-import { useRef } from 'react';
-
-import ContactsService from '../../services/ContactsService';
-import toast from '../../utils/toast';
-
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
+import useNewContact from './useNewContact';
 
 export default function NewContact() {
-  const contactFormRef = useRef(null);
-
-  async function handleSubmit(contact) {
-    try {
-      await ContactsService.createContact(contact);
-
-      contactFormRef.current.resetFields();
-
-      toast({
-        type: 'success',
-        text: 'Contato cadastrado com sucesso!',
-        duration: 3000,
-      });
-    } catch {
-      toast({
-        type: 'danger',
-        text: 'Ocorreu um erro ao cadastrar o contato!',
-        duration: 3000,
-      });
-    }
-  }
+  const {
+    contactFormRef,
+    handleSubmit,
+  } = useNewContact();
 
   return (
     <>
