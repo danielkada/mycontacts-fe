bd04031c95e072dc36126d6b7c674990870246c3
Kadã <dgames201615@outlook.com>
2022-12-22 20:28:12 -0300

    applying animation hook to loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -3,15 +3,18 @@ import PropTypes from 'prop-types';
 import { Overlay } from './styles';
 import Spinner from '../Spinner';
 import ReactPortal from '../ReactPortal';
+import useAnimatedUnmont from '../../hooks/useAnimatedUnmont';
 
 export default function Loader({ isLoading }) {
-  if (!isLoading) {
+  const { shouldRender, animatedElementRef } = useAnimatedUnmont(isLoading);
+
+  if (!shouldRender) {
     return null;
   }
 
   return (
     <ReactPortal containerId="loader-root">
-      <Overlay>
+      <Overlay isLeaving={!isLoading} ref={animatedElementRef}>
         <Spinner size={90} />
       </Overlay>
     </ReactPortal>
