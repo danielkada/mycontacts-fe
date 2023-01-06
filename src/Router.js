400a8081d8e641578929def2314c857a6a3b4b3c
Kadã <dgames201615@outlook.com>
2023-01-05 21:06:37 -0300

    declaring routes with useRoutes

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,24 @@
-import React from 'react';
-
-import { Routes, Route } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import NewContact from './pages/NewContact';
 import EditContact from './pages/EditContact';
 
 export default function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<NewContact />} />
-      <Route path="/edit/:id" element={<EditContact />} />
-    </Routes>
-  );
+  const routes = useRoutes([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/new',
+      element: <NewContact />,
+    },
+    {
+      path: '/edit/:id',
+      element: <EditContact />,
+    },
+  ]);
+
+  return routes;
 }
